8afa572490f9376d103eb968e9ce8f6630c02c91
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-02-09 22:19:50 +0530

    change role

--- src/NameCard.jsx
+++ src/NameCard.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 const NameCard = () => {
-let [Name, setName] = useState("Harmeet")
-let changeName=()=>{
-  Name="React Master 🚀"
-  setName(Name)
-}
+  let [Name, setName] = useState("Harmeet")
+  let changeName = () => {
+    Name = "React Master 🚀"
+    setName(Name)
+  }
   return (
     <div className=' flex flex-col gap-4 p-5 bg-white w-1/2 h-80 rounded-xl items-start'>
       <h1 className='text-xl font-bold'>Name Card</h1>
       <h1 className='text-2xl' >{Name}</h1>
-      <button onClick={()=>{changeName()}} className='px-3.5 text-md py-2 text-white rounded-md bg-green-500'>Change Name</button>
+      <button onClick={() => { changeName() }} className='px-3.5 text-md py-2 text-white rounded-md bg-green-500'>Change Name</button>
     </div>
   )
 }
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UserCard = () => {
+  let [role, setrole] = useState("Instructor")
+  let changerole=()=>{
+    role="Admin"
+    setrole(role)
+  }
   return (
     <div className=' flex flex-col gap-4 p-5  bg-white w-1/2 h-80 rounded-xl items-start'>
       <h1 className='text-xl font-bold'>User Card</h1>
       <div >
 
-      <h1>Name: Devendra</h1>
-      <h1>Role: Admin</h1>
+      <h1>Name: Harmeet</h1>
+      <h1>Role: {role}</h1>
       </div>
-          <button className='px-3.5 text-md py-2 text-white rounded-md bg-yellow-500'>Change Name</button>
+          <button onClick={()=>{changerole()}} className='px-3.5 text-md py-2 text-white rounded-md bg-yellow-500'>Change Name</button>
     </div>
   )
 }
